e5b89edcfb32bcce4bec105373b11229cd4d9c93
s1f10210273 <s1f102102739@iniad.org>
2025-02-04 23:29:09 +0900

    メインコンポーネント

---
+++ components/board.tsx
@@ -0,0 +1,9 @@
+export default function Board() {
+  return (
+    <>
+      <div className="w-full h-full flex justify-center items-center">
+        <h1>マインスイーパー</h1>
+      </div>
+    </>
+  );
+}
